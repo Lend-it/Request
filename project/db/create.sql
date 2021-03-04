68b222e7c97043b44c0bb509191229d06310049c
Lucas Dutra <ldutra98@gmail.com>
2021-03-04 14:53:38 -0300

    db: Update database creation script
    Co-authored-by: Rogerio Junior <junior.rogerio8@gmail.com>
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- project/db/create.sql
+++ project/db/create.sql
@@ -1,3 +1,13 @@
-CREATE DATABASE request_prod;
-CREATE DATABASE request_dev;
-CREATE DATABASE request_test;
+CREATE DATABASE requestLendItDB_dev
+    WITH
+        ENCODING = UTF8
+        LC_COLLATE = 'pt_BR.UTF-8'
+        LC_CTYPE = 'pt_BR.UTF-8'
+        TEMPLATE = template0;
+
+CREATE DATABASE requestLendItDB_test
+    WITH
+        ENCODING = UTF8
+        LC_COLLATE = 'pt_BR.UTF-8'
+        LC_CTYPE = 'pt_BR.UTF-8'
+        TEMPLATE = template0;
