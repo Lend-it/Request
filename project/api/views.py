ca4c7a3716818961133d05c738578ffe16ec7e7a
Thais-ra <rotinasoftware@gmail.com>
2021-03-18 10:37:35 -0300

    feat: fga-eps-mds/2020.2-Lend.it#93 Creation of the post method
    Co-authored-by: Matheus Monteiro <matheusyanmonteiro@gmail.com>

--- project/api/views.py
+++ project/api/views.py
@@ -4,7 +4,9 @@ from sqlalchemy import exc
 
 from project.api.models import Request
 from project.api.models import Category
-from project import db
+from database_singleton import Singleton
+
+db = Singleton().database_connection()
 
 category_blueprint = Blueprint("categories", __name__)
 
@@ -13,6 +15,34 @@ category_blueprint = Blueprint("categories", __name__)
 def get_all_request():
     response = {
         "status": "success",
-        "data": {"categories": [request.to_json() for request in Category.query.all()]},
+        "data": {
+            "categories": [category.to_json() for category in Category.query.all()]
+        },
     }
     return jsonify(response), 200
+
+
+@category_blueprint.route("/api/product_category", methods=["POST"])
+def add_categories():
+    post_data = request.get_json()
+
+    error_response = {"status": "fail", "message": "Invalid payload."}
+
+    if not post_data:
+        return jsonify(error_response), 400
+
+    name = post_data.get("name")
+
+    try:
+        db.session.add(Category(name))
+        db.session.commit()
+
+        response = {
+            "status": "success",
+            "data": {"message": f"Category {name} was created!"},
+        }
+
+        return jsonify(response), 201
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        return jsonify(error_response), 400
