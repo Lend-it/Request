4a554368a9f998ab20f3e878ffe13c420b64537f
Thais-ra <rotinasoftware@gmail.com>
2021-03-20 14:49:50 -0300

    test: fga-eps-mds/2020.2-Lend.it#93 Test refatoring

--- project/tests/test_product_category.py
+++ project/tests/test_product_category.py
@@ -1,47 +1,34 @@
 import json
 import unittest
 
-from datetime import datetime
 from project.tests.base import BaseTestCase
 from database_singleton import Singleton
+
+from project.api.models import Category
 
 db = Singleton().database_connection()
 
 
+def add_category(name):
+    category = Category(name)
+    db.session.add(category)
+    db.session.commit()
+    return category
+
+
 class TestProductCategory(BaseTestCase):
     def test_get_all_category(self):
+        add_category("Eletrodomésticos")
+        add_category("Livros e revistas")
+        add_category("Eletrônicos")
+
         with self.client:
-
-            self.client.post(
-                "/product_category",
-                data=json.dumps({"name": "Eletrodomésticos"}),
-                content_type="application/json",
-            )
-
-            self.client.post(
-                "/product_category",
-                data=json.dumps({"name": "Livros e revistas"}),
-                content_type="application/json",
-            )
-
-            self.client.post(
-                "/product_category",
-                data=json.dumps({"name": "Eletrônicos"}),
-                content_type="application/json",
-            )
-
             response = self.client.get("/product_category")
             data = json.loads(response.data.decode())
-            print(data)
 
             self.assertEqual(response.status_code, 200)
             self.assertIn("success", data["status"])
 
-            self.assertEqual(1, data["data"]["categories"][0]["productcategoryid"])
             self.assertIn("Eletrodomésticos", data["data"]["categories"][0]["name"])
-
-            self.assertEqual(2, data["data"]["categories"][1]["productcategoryid"])
             self.assertIn("Livros e revistas", data["data"]["categories"][1]["name"])
-
-            self.assertEqual(3, data["data"]["categories"][2]["productcategoryid"])
             self.assertIn("Eletrônicos", data["data"]["categories"][2]["name"])
