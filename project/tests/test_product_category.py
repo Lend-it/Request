21bc8656b71418807ee2e57b106a74a4ce1e057c
Thais-ra <rotinasoftware@gmail.com>
2021-03-31 16:06:57 -0300

    test: fga-eps-mds/2020.2-Lend.it#124 Create a test to add_categories

--- project/tests/test_product_category.py
+++ project/tests/test_product_category.py
@@ -7,6 +7,22 @@ from project.tests.utils import add_category
 
 
 class TestProductCategory(BaseTestCase):
+    def test_add_categories(self):
+        with self.client:
+            response = self.client.post(
+                '/product_category',
+                data = json.dumps({
+                    'name': 'Jogos'
+                }),
+                content_type='application/json',
+            )
+
+            data = json.loads(response.data.decode())
+
+            self.assertEqual(response.status_code, 201)
+            self.assertIn("Category Jogos was created!", data['data']['message'])
+            self.assertIn('success', data['status'])
+
     def test_get_all_category(self):
         add_category("Eletrodomésticos")
         add_category("Livros e revistas")
@@ -19,6 +35,8 @@ class TestProductCategory(BaseTestCase):
             self.assertEqual(response.status_code, 200)
             self.assertIn("success", data["status"])
 
-            self.assertIn("Eletrodomésticos", data["data"]["categories"][0]["name"])
-            self.assertIn("Livros e revistas", data["data"]["categories"][1]["name"])
-            self.assertIn("Eletrônicos", data["data"]["categories"][2]["name"])
+            self.assertIn("Eletrodomésticos", data["data"]["categories"][1]["name"])
+            self.assertIn("Livros e revistas", data["data"]["categories"][2]["name"])
+            self.assertIn("Eletrônicos", data["data"]["categories"][3]["name"])
+
+
